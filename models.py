af50d4f6cdb7cd8a2afcc2dfbf2e3d1663817553
Andrey <152691772+cycler11@users.noreply.github.com>
2025-05-31 18:12:53 +0300

    Update models.py

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     role = db.Column(db.String(20), nullable=False)
 
     @property
