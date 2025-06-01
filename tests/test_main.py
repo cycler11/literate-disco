464e29a2b9a3839a04c40fc798c247693a45e01e
Andrey <152691772+cycler11@users.noreply.github.com>
2025-06-01 18:28:24 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,5 @@
 def test_index_route(app, client, init_database):
-    """
-    Проверяем, что корневой маршрут "/" возвращает 200 и отдаёт HTML.
-    """
+
     resp = client.get('/')
     assert resp.status_code == 200
-    assert b'<html' in resp.data, "Ожидается HTML на странице '/'"
+    assert b'<html' in resp.data, "HTML '/'"
